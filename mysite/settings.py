9796227817feeb399b7458158c4812a9522b0907
Saeideh Nezhad <sepide.eslamnezhad@gmail.com>
2026-03-16 23:31:06 +0100

    add back just sqlite

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,21 +85,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # Database configuration for local (SQLite) and Render (PostgreSQL)
-if "RENDER" in os.environ:
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=os.environ.get("DATABASE_URL"),
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 
 # Password validation
